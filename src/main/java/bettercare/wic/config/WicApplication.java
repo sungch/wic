b10597e939097d06d7221137b7275dd32030b94e
sung <sungch@ldschurch.org>
2018-12-03 20:26:24 -0700

    handles CORS in endpoints. It is not working yet.

--- src/main/java/bettercare/wic/config/WicApplication.java
+++ src/main/java/bettercare/wic/config/WicApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = "bettercare.wic")
 public class WicApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WicApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WicApplication.class, args);
+    }
 }
